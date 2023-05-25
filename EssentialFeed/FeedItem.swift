fd07283ec8f793381e001e22439a12968bae58fd
Ignacio Esau Nevarez Zuñiga <INevarezZuniga@rccl.com>
2023-05-24 20:58:27 -0600

    Request data from URL on `load`

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ignacio Esau Nevarez Zuniga on 04/05/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
---
+++ EssentialFeedTests/RemoteFeedLaoderTests.swift
@@ -0,0 +1,39 @@
+//
+//  RemoteFeedLaoderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Ignacio Esau Nevarez Zuniga on 04/05/23.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
+}
+
+class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
+    }
+}
